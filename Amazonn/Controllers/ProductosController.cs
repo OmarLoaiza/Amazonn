427fb6c183508a0a7cb8b7d438b33a9a328a2d90
Omar Loaiza <omar.loaiza@uabc.edu.mx.com>
2023-10-03 16:05:08 -0700

    Add project files.

---
+++ Amazonn/Controllers/ProductosController.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace Amazonn.Controllers
+{
+    public class ProductosController : Controller
+    {
+        // GET: Productos
+        public ActionResult Index()
+        {
+            return View();
+        }
+
+
+
+    }
+}
---
+++ Amazonn/Startup.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(Amazonn.Startup))]
+namespace Amazonn
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+        }
+    }
+}
